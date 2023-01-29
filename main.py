473cb1b81fd5138a9d9582353a90e4b8613137c0
Farruh <dreamquestff@gmail.com>
2023-01-29 11:45:20 +0500

    добавил пару модулей

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-from aiohttp import web  # основной модуль aiohttp
-import jinja2  # шаблонизатор jinja2
-import aiohttp_jinja2  # адаптация jinja2 к aiohttp
+import aiohttp_jinja2
+import jinja2
+from aiohttp import web
 
-# в этой функции производится настройка url-путей для всего приложения
+from app.settings import config, BASE_DIR
+from app.store.database.accessor import PostgresAccessor
+
+
+def setup_config(application):
+    application["config"] = config
+
+
+def setup_external_libraries(application):
+    aiohttp_jinja2.setup(
+        application,
+        loader=jinja2.FileSystemLoader(f"{BASE_DIR}/templates"),
+    )
+
+
+# настроим url-пути для доступа к нашему будущему приложению
 def setup_routes(application):
-   from app.forum.routes import setup_routes as setup_forum_routes
-   setup_forum_routes(application)  # настраиваем url-пути приложения forum
+    from app.forum.routes import setup_routes as setup_forum_routes
+    setup_forum_routes(application)
 
-def setup_external_libraries(application: web.Application) -> None:
-   # указываем шаблонизатору, что html-шаблоны надо искать в папке templates
-   aiohttp_jinja2.setup(application, loader=jinja2.FileSystemLoader("templates"))
+
+def setup_accessors(application):
+    application['db'] = PostgresAccessor()
+    application['db'].setup(application)
+
 
 def setup_app(application):
-   # настройка всего приложения состоит из:
-   setup_external_libraries(application)  # настройки внешних библиотек, например шаблонизатора
-   setup_routes(application)  # настройки роутера приложения
+    setup_config(application)
+    setup_accessors(application)
+    setup_external_libraries(application)
+    setup_routes(application)
 
-app = web.Application()  # создаем наш веб-сервер
 
-if __name__ == "__main__":  # эта строчка указывает, что данный файл можно запустить как скрипт
-   setup_app(app)  # настраиваем приложение
-   web.run_app(app)  # запускаем приложение
+app = web.Application()  # инстанцируем наш веб-сервер
+
+if __name__ == "__main__":
+    setup_app(app)
+    web.run_app(app, port=config["common"]["port"])
